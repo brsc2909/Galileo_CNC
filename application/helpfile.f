bfb64f8f8c84b923c536de34f226c719dee3583e
brsc2909 <brsc2909@gmail.com>
2015-10-27 11:52:11 +0000

    wind

---
+++ application/helpfile.f
@@ -0,0 +1,7 @@
+Galileo CNC
+
+getg		reads a specified g code file line by line
+sendserial	send serial through the defined serial port
+readserial	read serail message from defined serial port
+help		displays help text
+setup		set up machine for job 
